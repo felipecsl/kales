97fd495a492c80df797eb4552322f2c267e6e76a
Felipe Lima <felipe.lima@gmail.com>
2019-02-15 19:33:46 -0800

    Fix website URL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Kales', // Title for your website.
   tagline: 'Move fast with confidence',
-  url: 'https://felipecsl.github.io', // Your website URL
+  url: 'https://felipecsl.github.io/kales', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
