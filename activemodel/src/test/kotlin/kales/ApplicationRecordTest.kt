3b6e52e5ba6a4b8e6de7dc66db9f26fe712019af
Felipe Lima <felipe.lima@gmail.com>
2019-02-20 22:23:00 -0800

    moar tests

--- activemodel/src/test/kotlin/kales/ApplicationRecordTest.kt
+++ activemodel/src/test/kotlin/kales/ApplicationRecordTest.kt
@@ -4,9 +4,8 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class ApplicationRecordTest {
-  @Test fun `no records`() {
-    ApplicationRecord.JDBI.withHandle<Any, RuntimeException> {
-      it.execute("CREATE TABLE testmodels (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR)")
+  @Test fun `test no records`() {
+    withTestDb {
       assertThat(TestModel.all()).isEmpty()
       assertThat(TestModel.where("id" to 1)).isEmpty()
       assertThat(TestModel.find(1)).isNull()
@@ -14,19 +13,44 @@ class ApplicationRecordTest {
     }
   }
 
-  @Test fun `test model create and query`() {
-    ApplicationRecord.JDBI.withHandle<Any, RuntimeException> {
-      it.execute("CREATE TABLE testmodels (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR)")
+  @Test fun `test all`() {
+    withTestDb {
       TestModel.create("name" to "Hello World")
       TestModel.create("name" to "Ping Pong")
       val expectedModel1 = TestModel(1, "Hello World")
       val expectedModel2 = TestModel(2, "Ping Pong")
       assertThat(TestModel.all()).containsExactly(expectedModel1, expectedModel2)
+    }
+  }
+
+  @Test fun `test where`() {
+    withTestDb {
+      TestModel.create("name" to "Hello World")
+      TestModel.create("name" to "Ping Pong")
+      val expectedModel1 = TestModel(1, "Hello World")
+      val expectedModel2 = TestModel(2, "Ping Pong")
       assertThat(TestModel.where("name" to "Hello World")).containsExactly(expectedModel1)
+      assertThat(TestModel.where("id" to 1, "name" to "Hello World")).containsExactly(expectedModel1)
       assertThat(TestModel.where("id" to 1)).containsExactly(expectedModel1)
       assertThat(TestModel.where("id" to 2)).containsExactly(expectedModel2)
-      assertThat(TestModel.find(1)).isEqualTo(expectedModel1)
-      assertThat(TestModel.find(2)).isEqualTo(expectedModel2)
+      assertThat(TestModel.where("id" to 3)).isEmpty()
+    }
+  }
+
+  @Test fun `test find`() {
+    withTestDb {
+      TestModel.create("name" to "Hello World")
+      TestModel.create("name" to "Ping Pong")
+      assertThat(TestModel.find(1)).isEqualTo(TestModel(1, "Hello World"))
+      assertThat(TestModel.find(2)).isEqualTo(TestModel(2, "Ping Pong"))
+      assertThat(TestModel.find(3)).isNull()
+    }
+  }
+
+  private fun withTestDb(block: () -> Unit) {
+    ApplicationRecord.JDBI.withHandle<Any, RuntimeException> {
+      it.execute("CREATE TABLE testmodels (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR)")
+      block()
     }
   }
 }
