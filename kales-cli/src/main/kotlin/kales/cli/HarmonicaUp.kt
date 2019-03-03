771ece0c89cf57cadee71d8674f9868a0e57f162
Felipe Lima <felipe.lima@gmail.com>
2019-03-02 19:51:08 -0800

    Automatically inject migration instantiation before eval

--- kales-cli/src/main/kotlin/kales/cli/HarmonicaUp.kt
+++ kales-cli/src/main/kotlin/kales/cli/HarmonicaUp.kt
@@ -1,10 +1,11 @@
 package kales.cli
 
-import com.improve_future.harmonica.core.AbstractMigration
 import com.improve_future.harmonica.core.Connection
 import com.improve_future.harmonica.service.VersionService
-import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
+import kales.migrations.Migration
+import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngine
 import java.io.File
+import javax.script.ScriptEngineManager
 
 internal class HarmonicaUp(
     private val migrationsDirectory: File,
@@ -32,15 +33,34 @@ internal class HarmonicaUp(
     }
   }
 
-  private fun readMigration(script: String) =
-      engine.eval(removePackageStatement(script)) as AbstractMigration
+  private fun readMigration(script: String): Migration {
+    val finalScript = addMigrationIntantiation(removePackageStatement(script))
+    println("final script: $finalScript")
+    return engine.eval(finalScript) as Migration
+  }
 
   private companion object {
     val engine by lazy {
-      KotlinJsr223JvmLocalScriptEngineFactory().scriptEngine
+      ScriptEngineManager().getEngineByName("kotlin") as KotlinJsr223JvmLocalScriptEngine
+    }
+
+    /**
+     * Apparently the Kotlin ScriptEngine needs an expression at the end of the file in order to
+     * be able to return the class object after evaluating the migration file. So, we'll manually
+     * scan for the migration class name and inject an instantiation at the end of the file.
+     */
+    private fun addMigrationIntantiation(script: String): String {
+      val regex = "class ([a-zA-z0-9]+)".toRegex()
+      for (line in script.lines()) {
+        regex.find(line)?.let { matchResult ->
+          val className = matchResult.groups[1]!!.value
+          return "$script\n$className()"
+        }
+      }
+      throw IllegalArgumentException("No class name found on migration file")
     }
 
     private fun removePackageStatement(script: String) =
-        script.replace(Regex("^\\s*package\\s+.+"), "")
+        script.replace("^\\s*package\\s+.+".toRegex(), "")
   }
 }
