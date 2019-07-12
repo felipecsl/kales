fecde1183b546e0fbe4fa648ef07dd0e4577ca39
Felipe Lima <felipe.lima@gmail.com>
2019-07-11 22:29:34 -0700

    Fix ktlint

--- kales/src/test/kotlin/kales/test/app/controllers/TestController.kt
+++ kales/src/test/kotlin/kales/test/app/controllers/TestController.kt
@@ -1,7 +1,6 @@
 package kales.test.app.controllers
 
 import io.ktor.application.ApplicationCall
-import io.ktor.http.HttpMethod.Companion.Post
 import io.ktor.request.receiveParameters
 import kales.actionpack.ApplicationController
 import kales.test.app.views.test.CreateViewModel
