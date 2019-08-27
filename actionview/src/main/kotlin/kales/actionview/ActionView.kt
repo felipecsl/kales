b5a48fd6badf8e31abf30004d65f6d146c7e28f9
Felipe Lima <felipe.lima@gmail.com>
2019-08-26 23:27:47 -0700

    comment nits

--- actionview/src/main/kotlin/kales/actionview/ActionView.kt
+++ actionview/src/main/kotlin/kales/actionview/ActionView.kt
@@ -5,10 +5,7 @@ import kales.actionpack.KalesApplicationCall
 import kales.actionpack.ViewModel
 import kotlinx.html.FlowContent
 
-// Need `flash` here, but dont have at the time of View instantiation, especially when it is
-// explicitly created during the action execution (we have otherwise when its reflectively
-// instantiated via inference in KalesApplication.)
-// Maybe inject KalesApplicationCall in here too??
+/** TODO document public API */
 abstract class ActionView<T : ViewModel>(
   protected val call: KalesApplicationCall,
   protected val bindings: T? = null
